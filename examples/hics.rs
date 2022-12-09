c8ad7a431df86838f741e8989ec7e46865798932
aronpaulson <ap@shtsoft.eu>
2022-12-09 01:38:24 +0100

    Comment hics example

--- examples/hics.rs
+++ examples/hics.rs
@@ -1,3 +1,52 @@
+//&This example implements a Heat Index Control System (hics).
+//&More precisely, the example implements a system to keep the heat index (a quantity depending on the real temperature and the humidity via a certain mapping) in a room located in a region with a hot and damp climate bearable.\
+//&The system specification is that the heat index has to be brought down periodically to a certain value within a window of tolerance depending on the daytime by first trying to dehumidify the room and if that does not suffice to also cool down the room.
+//&Furthermore, a thermohygrometer and a clock shall be accessible for an implementation.\
+//&The hics-implementation presented in this example measures temeprature and humidity as well as the time to decide if it has to take action and if it actuates something it waits for it to take effect in order to decide whether to repeat or to go idle for a period.
+//&Moreover it follows the structuring-suggestions from [Why Functional Programming Matters](https://www.cse.chalmers.se/~rjmh/Papers/whyfp.pdf).
+//&There they describe how the use of higher-order functions and lazy evaluation can greatly improves the modularity of programs.
+//&They illustrate that by modularizing on demand computations of perhaps infinite objects like real numbers and game trees exploiting that in languages with first-class functions and lazy evaluation everything is a generator in some sense.
+//&As the hics described above shares that 'on demand computation'-aspect - most notably, it measures (that is, reads out the thermohygrometer) on demand - these modularization techniques will apply in that case, too.
+//&So adapting the techniques specifically to rspl and rust yields a modular implementation of the hics in rust.
+//&
+//&The intention of the example is to demonstrate rspl's applicability in demand-driven[^1] programming.
+//&
+//&[^1]: Look at [Codata in Action](https://www.microsoft.com/en-us/research/uploads/prod/2020/01/CoDataInAction.pdf) for some more explanation on that term.
+//&
+//&Now that we have said what we are going to implement and why let us explain our techniques before
+//&presenting the code applying those techniques.
+//&To this end we split the following discussion in two parts.
+//&First, we introduce the general design pattern of encoding generators in rspl.
+//&This is done without referring to the specific example of control systems.
+//&Then, second, before discussing the actual hics code in place we briefly discuss the code's overall structure.\
+//&So, first, rspl's stream processors can encode some sort of generator: regarding a sufficiently general definition of generator one can consider any stream processor a generator because output is generated on demand in an incremental manner.
+//&But even for the more specific definition of generators as functions which can remember state infromation between calls rspl's stream processors offer an encoding.
+//&To understand how, first note that rspl's stream processors would implement the `Fn`-trait if rust allowed users to arbitrarily implement that trait.
+//&This is because implementing a stream processor from `A` to `B` corresponds to defining a function from `Stream<A>` to `Stream<B>`.
+//&So, if `A` is the input signature and `B` the yield type of a generator then that generator could be encoded as stream processor from `A` to `B` provided that a way to remember state information is available.
+//&However, the perhaps most common approach to state in functional programming - and rspl is functional programming - is state-passing-style[^2].
+//&[^2]: Also see the concept of monads which kind of subsumes foobar-passing-style.
+//&And, in fact, state-passing is applicable to rspl's stream processors.
+//&One way is to construct a stream processor by a rust function with a single parameter representing the state returning a stream processor capturing that state within a (lazy) recursive call.
+//&The returned stream processor of such a function is a generator and the pattern of such a function is as follows:
+//&```rust
+//&fn generator<'a, S, A, B>(state: S) -> StreamProcessor<'a, A, B> {
+//&    ...;
+//&    StreamProcessor::get(|a: A| StreamProcessor::put(..., || generator(state)))
+//&}
+//&```
+//&Here, the dots are supposed to be replaced by the generators body.
+//&(Note that if `A` is `()` it can make sense to omit the `get`-part.)\
+//&After having discussed the encoding of generators as stream processors let us have a look at the structure of our hics implementation.
+//&Essentially, it consists of four parts.
+//&The first is a module encapsulating general aspects of control systems.
+//&The second and the third part specialize to and use those aspects for heat index controlling.
+//&Particularly, the second part implements the control system interface while the third part is a driver responsible for executing that implementation according to the measure-on-demand strategy.
+//&Finally, the fourth part is the main-function simulating the hics environment and setting up the driver for the hics.
+//&
+//&Let us now walk through the code together.
+
+//&```rust
 mod control {
     use rspl::streams::infinite_lists::InfiniteList;
     use rspl::streams::Stream;
@@ -6,6 +55,8 @@ mod control {
     use std::thread;
     use std::time::Duration;
 
+    // This is a definition of control system. Importantly, it requires a `meter` to generate
+    // measurements on demand and that this is statically enforced by the typing.
     pub trait System<'a, Space> {
         fn meter(&self) -> StreamProcessor<'a, (), Space>;
         fn reference(&self) -> f64;
@@ -25,9 +76,11 @@ mod control {
         fn execute(self, mut cs: impl System<'a, Space>, epsilon: f64) {
             let mut status;
 
+            // Here the measurements are generated (lazily).
             let mut positions = cs.meter().eval(InfiniteList::constant(()));
 
             loop {
+                // Here the actual measurement is made.
                 positions = positions.tail();
                 let position = *positions.head();
 
@@ -61,6 +114,7 @@ use std::time::Duration;
 use crossbeam::channel;
 use crossbeam::channel::Sender;
 
+// This constant is the window of tolerance for the heat index.
 const EPSILON: f64 = 0.5;
 
 const REFERENCE_HEAT_INDEX_DAY: f64 = 91.0;
@@ -81,8 +135,10 @@ const NATURAL_INCREASE: HeatIndexSpace = HeatIndexSpace {
     humidity: 0.1,
 };
 
+// This block defines time-related constants. In particular, note that the `TICK` is intended to 
+// represent 10 real seconds.
 const TICK_LENGTH: u64 = 5; // in (real) millis
-const TICK: u64 = 1; // shall represent 10 (real) seconds
+const TICK: u64 = 1;
 const DAY: u64 = 8640 * TICK;
 const DWELL_TIME: u64 = 6 * TICK;
 const CONTROL_PERIOD: u64 = 180 * TICK;
@@ -97,16 +153,19 @@ type Clock = AtomicU64;
 
 #[derive(Copy, Clone)]
 struct HeatIndexSpace {
-    temperature: f64, // degree Fahrenheit
-    humidity: f64,    // percent
+    temperature: f64, // in degree Fahrenheit
+    humidity: f64,    // in percent
 }
 
+// This type defines the output signals of the hics. A signal is either status information
+// (`Show(...)`) or orders for the actuator to execute (`Dehumidfy` and `Cool`).
 enum HeatIndexSignal {
     Show(Time, HeatIndex),
     Dehumidify,
     Cool,
 }
 
+// This type is the actual hics. Essentially, it is the communication interface to its environment.
 #[derive(Clone)]
 struct Hics {
     clock_finger: Arc<Clock>,
@@ -135,7 +194,7 @@ impl<'a> control::System<'a, HeatIndexSpace> for Hics {
         }
     }
     fn quantity(&self, position: HeatIndexSpace) -> f64 {
-        // the body is the heat index formula from https://en.wikipedia.org/wiki/Heat_index
+        // The body is the heat index formula from https://en.wikipedia.org/wiki/Heat_index.
         const C_1: f64 = -42.379;
         const C_2: f64 = 2.049_015_23;
         const C_3: f64 = 10.143_331_27;
@@ -191,10 +250,13 @@ fn driver(hics: Hics) {
 
     assert!(UNSAFE_BARRIER > SERVICE_BARRIER);
 
+    // Here the runs of the hics are generated (lazily).
     let mut runs = control(hics, 0).eval(InfiniteList::constant(()));
+
     loop {
         thread::sleep(Duration::from_millis(CONTROL_PERIOD * TICK_LENGTH));
 
+        // Here an interation of the hics is started.
         runs = runs.tail();
         let run_count = *runs.head();
 
@@ -215,7 +277,8 @@ fn main() {
         let red = |x| (x * 16.0 - 1350.0) as u8;
         let green = 25;
         let blue = |x| (1450.0 - x * 16.0) as u8;
-        // The cryptic part of the following `format!(...)` is just an ANSI escape code to get `format!({:.1}°F, heat_index)` with a truecolor R(ed)G(reen)B(lue) background.
+        // The cryptic part of the following `format!(...)` is just an ANSI escape code to get
+        // `format!({:.1}°F, heat_index)` with a truecolor R(ed)G(reen)B(lue) background.
         let degree = format!(
             "\x1b[48;2;{};{};{}m{:.1}°F\x1b[0m",
             red(heat_index),
@@ -256,6 +319,7 @@ fn main() {
     let _thermohygrometer_simulator = thread::spawn(move || {
         let thermohygrometer_finger = Arc::clone(&thermohygrometer);
 
+        // This is the climate effect actuated by the hics.
         let _actuator_simulator = thread::spawn(move || loop {
             let signal = signals_r.recv().unwrap();
 
@@ -267,6 +331,7 @@ fn main() {
             }
         });
 
+        // This is the climate effect actuated by nature.
         loop {
             thread::sleep(Duration::from_millis(NATURAL_INCREASE_PERIOD * TICK_LENGTH));
 
@@ -278,3 +343,11 @@ fn main() {
 
     driver(hics);
 }
+//&```
+
+//&Finally, let us conclude with the key take-away:
+//&rspl can encode generators and is hence suited for demand-driven programming.
+//&However, it is not so clear why to use rspl to encode generators in general.
+//&Indeed, this is also not quite what we wanted to show.
+//&The idea is rather to show that rspl's stream processors can naturally incorporate demand-driven programming making them particularly useful to stream processing problems with demand-driven aspects.
+//&It might be that the hics implemented here is not the best possible example to do so but the best we could come up with as of yet which is real-world enough while still being focused on the `put`-construct of rspl.
